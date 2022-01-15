3be06c53df8e7c6e2b8656e53530fcb073769565
Paddy <widom3288@gmail.com>
2022-01-15 19:28:14 +0800

    重构

--- webpack.config.js
+++ webpack.config.js
@@ -42,13 +42,14 @@ const frontendDirectory = "ArtNfts_assets";
 
 const asset_entry = path.join("src", frontendDirectory, "src", "index.html");
 
+
 module.exports = {
   target: "web",
   mode: isDevelopment ? "development" : "production",
   entry: {
     // The frontend.entrypoint points to the HTML file for this build, so we need
     // to replace the extension to `.js`.
-    index: path.join(__dirname, asset_entry).replace(/\.html$/, ".js"),
+    index: path.join(__dirname, asset_entry).replace(/\.html$/, ".jsx"),
   },
   devtool: isDevelopment ? "source-map" : false,
   optimization: {
@@ -70,17 +71,14 @@ module.exports = {
     path: path.join(__dirname, "dist", frontendDirectory),
   },
 
-  // Depending in the language or framework you are using for
-  // front-end development, add module loaders to the default
-  // webpack configuration. For example, if you are using React
-  // modules and CSS as described in the "Adding a stylesheet"
-  // tutorial, uncomment the following lines:
-  // module: {
-  //  rules: [
-  //    { test: /\.(ts|tsx|jsx)$/, loader: "ts-loader" },
-  //    { test: /\.css$/, use: ['style-loader','css-loader'] }
-  //  ]
-  // },
+
+  module: {
+    rules: [
+      { test: /\.(ts|tsx|jsx)$/, loader: "ts-loader" },
+      //  { test: /\.css$/, use: ['style-loader','css-loader'] }
+    ]
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, asset_entry),
